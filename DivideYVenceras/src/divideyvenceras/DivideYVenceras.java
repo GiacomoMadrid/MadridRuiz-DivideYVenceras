525a67ddca85e1b1406076a811b3db9c72284dd9
GiacomoMadrid <gsmr.18@gmail.com>
2025-05-22 02:00:59 -0500

    Test

--- DivideYVenceras/src/divideyvenceras/DivideYVenceras.java
+++ DivideYVenceras/src/divideyvenceras/DivideYVenceras.java
@@ -4,6 +4,11 @@
  */
 package divideyvenceras;
 
+import java.util.Scanner;
+import modelo.AckermannIterativa;
+import modelo.AckermannRecursiva;
+import modelo.MedidorTiempo;
+
 /**
  *
  * @author Giacomo
@@ -14,7 +19,29 @@ public class DivideYVenceras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el valor de m: ");
+        int m = scanner.nextInt();
+
+        System.out.print("Ingrese el valor de n: ");
+        int n = scanner.nextInt();
+
+        System.out.println("\n== Función de Ackermann ==");
+
+        MedidorTiempo.medir(() -> {
+            try {
+                int resultado = AckermannRecursiva.calcular(m, n);
+                System.out.println("Resultado recursivo: " + resultado);
+            } catch (StackOverflowError e) {
+                System.out.println("¡Desbordamiento de pila en la versión recursiva!");
+            }
+        }, "Recursiva");
+
+        MedidorTiempo.medir(() -> {
+            int resultado = AckermannIterativa.calcular(m, n);
+            System.out.println("Resultado iterativa: " + resultado);
+        }, "Iterativa");
     }
     
 }
