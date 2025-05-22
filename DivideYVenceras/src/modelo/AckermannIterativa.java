510c6705bd7edd87d876536a6de1253af465853a
GiacomoMadrid <gsmr.18@gmail.com>
2025-05-22 01:59:48 -0500

    Iterativa

--- DivideYVenceras/src/modelo/AckermannIterativa.java
+++ DivideYVenceras/src/modelo/AckermannIterativa.java
@@ -4,10 +4,34 @@
  */
 package modelo;
 
+import java.util.Stack;
+
 /**
  *
  * @author Giacomo
  */
 public class AckermannIterativa {
-    
+
+    public static int calcular(int m, int n) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(m);
+
+        while (!stack.isEmpty()) {
+            m = stack.pop();
+
+            if (m == 0) {
+                n = n + 1;
+            } else if (n == 0) {
+                stack.push(m - 1);
+                n = 1;
+            } else {
+                stack.push(m - 1);
+                stack.push(m);
+                n = n - 1;
+            }
+        }
+
+        return n;
+    }
 }
